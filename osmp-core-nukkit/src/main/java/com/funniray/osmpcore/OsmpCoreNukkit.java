a4dbe9defa90615f692eb401c24809f28092e19a
Funniray <funniray10@gmail.com>
2019-12-22 10:46:19 -0500

    Initial Commit

---
+++ osmp-core-nukkit/src/main/java/com/funniray/osmpcore/OsmpCoreNukkit.java
@@ -0,0 +1,16 @@
+package com.funniray.osmpcore;
+
+import cn.nukkit.plugin.PluginBase;
+
+public final class OsmpCoreNukkit extends PluginBase {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
