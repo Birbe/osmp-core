50c555c25bbb3e50fca300a09732de8dc3bb09e5
Birbyy <erik_blair@outlook.com>
2019-12-26 23:52:04 +0100

    events work!

--- osmp-core-common/src/main/java/com/funniray/osmpcore/Event/EventBus.java
+++ osmp-core-common/src/main/java/com/funniray/osmpcore/Event/EventBus.java
@@ -12,27 +12,27 @@ import java.util.HashMap;
 
 public class EventBus {
 
-    protected HashMap<EventPriority, HashMap< Class<? extends IEvent>, ArrayList<Method>>> eventHandlers = new HashMap<>(); //Event priorities -> Event -> [Actual method handlers]
+    protected HashMap<EventPriority, HashMap< Class<? extends IEvent>, ArrayList<HandlerListenerPair>>> eventHandlers = new HashMap<>(); //Event priorities -> Event -> [Actual method handlers]
 
-    public void addEventHandler(EventPriority priority, Class<? extends IEvent> event, Method method) {
+    public void addEventHandler(EventPriority priority, Class<? extends IEvent> event, HandlerListenerPair pair) {
         if(!eventHandlers.containsKey(priority)) eventHandlers.put(priority, new HashMap<>());
         if(!eventHandlers.get(priority).containsKey(event)) eventHandlers.get(priority).put(event,new ArrayList<>());
-        eventHandlers.get(priority).get(event).add(method);
+        eventHandlers.get(priority).get(event).add(pair);
     }
 
     public IEvent handleEvent(IEvent event) {
         for(EventPriority priority : EventPriority.values()) {
             if(!eventHandlers.containsKey(priority)) continue;
-            HashMap< Class<? extends IEvent>, ArrayList<Method>> handlers = eventHandlers.get(priority);
+            HashMap< Class<? extends IEvent>, ArrayList<HandlerListenerPair>> handlers = eventHandlers.get(priority);
 
             Class implementedInterface = event.getClass().getInterfaces()[0];
-            ArrayList<Method> methods = handlers.get(implementedInterface);
+            ArrayList<HandlerListenerPair> methods = handlers.get(implementedInterface);
 
-            for(Method method : methods) {
+            for(HandlerListenerPair pair : methods) {
                 try {
-                    Parameter[] params = method.getParameters();
-                    Class target = params[0].getType();
-                    method.invoke(target.cast(event));
+                    Parameter[] params = pair.handler.getParameters();
+                    Class<?> target = params[0].getType().asSubclass(IEvent.class);
+                    pair.handler.invoke(pair.listener, target.cast(event));
                 } catch (IllegalAccessException | InvocationTargetException e) {
                     e.printStackTrace();
                 }
@@ -47,7 +47,10 @@ public class EventBus {
             Parameter[] params = method.getParameters();
             EventHandler annotation = method.getAnnotation(EventHandler.class);
             Class<? extends IEvent> target = params[0].getType().asSubclass(IEvent.class);
-            addEventHandler(annotation.priority(), target, method);
+            HandlerListenerPair pair = new HandlerListenerPair();
+            pair.listener = listener;
+            pair.handler = method;
+            addEventHandler(annotation.priority(), target, pair);
         }
     }
 }
---
+++ osmp-core-common/src/main/java/com/funniray/osmpcore/Event/HandlerListenerPair.java
@@ -0,0 +1,10 @@
+package com.funniray.osmpcore.Event;
+
+import java.lang.reflect.Method;
+
+public class HandlerListenerPair {
+
+    public EventListener listener;
+    public Method handler;
+
+}
